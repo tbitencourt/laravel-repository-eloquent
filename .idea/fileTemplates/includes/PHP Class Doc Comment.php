7fe79dd4b2a2bbe36cdc0442396579d82604408a
Thales Bitencourt <thalesbitencourt@gmail.com>
2020-04-17 19:58:25 -0300

    Updated PHP Storm File Templates Configuration

--- .idea/fileTemplates/includes/PHP Class Doc Comment.php
+++ .idea/fileTemplates/includes/PHP Class Doc Comment.php
@@ -5,6 +5,7 @@
 #if (${NAMESPACE}) * @package  ${NAMESPACE}
 #else * @package  LaravelRepositoryEloquent
 #end
+ * @author   Thales Bitencourt <thales.bitencourt@devthreads.com.br>
  * @author   DevThreads Team <contato@devthreads.com.br>
  * @license  https://www.devthreads.com.br  Copyright
  * @link     https://www.devthreads.com.br
